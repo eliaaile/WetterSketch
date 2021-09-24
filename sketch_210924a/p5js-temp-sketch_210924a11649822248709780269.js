7cb40d015995508c2addbd7916fb2f50411c9803
Elia El-Alam <elalamjuchlielia@gmail.com>
2021-09-24 15:02:49 +0200

    Regen hinzugefügt

--- sketch_210924a/p5js-temp-sketch_210924a11649822248709780269.js
+++ sketch_210924a/p5js-temp-sketch_210924a11649822248709780269.js
@@ -1,6 +1,7 @@
 function setup() {
 createCanvas(1000, 1000);
 background("blue");
+frameRate(60);
 
 fill(255, 255,0);
 strokeWeight(0);
@@ -11,7 +12,9 @@ fill("grey");
  ellipse(610,125,100,100);
  ellipse(790,125,100,100);
  rect(610,125,190,50);
+ 
 
+ 
 strokeWeight(10);
 stroke(255,255,0);
 
@@ -25,5 +28,23 @@ for(let i = 0; i<=16; i++) {
 
 
 
+
+}
+function draw() {
+  stroke(100, 100, 255)
+  strokeWeight(2);
+  
+  let x = random(550,850);
+  let y = random(180,800);
+  
+   line(x,y,x,y + 40);
+
+  stroke(0, 0, 255, 200);
+  strokeWeight(10);
+  
+  let x2 = random(550,850);
+  let y2 = random(180,800);
+  
+   line(x2,y2,x2,y2 + 40);
 
 }
--- sketch_210924a/sketch_210924a.js
+++ sketch_210924a/sketch_210924a.js
@@ -1,12 +1,20 @@
 function setup() {
 createCanvas(1000, 1000);
-background(153);
+background("blue");
+frameRate(1);
 
 fill(255, 255,0);
 strokeWeight(0);
 ellipse(0,0,300,300);
 
+fill("grey");
+ ellipse(700,100,100,100);
+ ellipse(610,125,100,100);
+ ellipse(790,125,100,100);
+ rect(610,125,190,50);
+ 
 
+ 
 strokeWeight(10);
 stroke(255,255,0);
 
@@ -14,10 +22,22 @@ for(let i = 0; i<=16; i++) {
     line(50, 0, 250, 0);
     rotate(2 * PI / 16 * i);
   }
+ 
 
 
 
 
+
+
+}
+function draw() {
+  stroke(100, 100, 255)
+  strokeWeight(2);
+  
+  let x = random(600,900);
+  let y = random(200,800);
+  
+   line(x,y,x,y + 40);
 
 
 }
